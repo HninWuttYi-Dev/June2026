7d3765701fe8569e7245ae767982e2741019991b
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2026-07-10 10:43:30 +0630

    implement User Model first

--- June2026.EntityFrameworkCore/UserEntity.cs
+++ June2026.EntityFrameworkCore/UserEntity.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace June2026.EntityFrameworkCore
 {
+    [Table("Tbl_User")]
     public class UserEntity
     {
-        
+        [Key]
+        [Column("userId")]
+        public int id {get; set;}
+        public string name {get; set;}
     }
 }
