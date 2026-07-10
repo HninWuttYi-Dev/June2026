cf368a0a958c75c22db28d4a9ead060629ce6b0b
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2026-07-10 18:45:38 +0630

    chore: scaffold EF Core database models and DbContext

---
+++ June2026.Database/AppDbContextModels/AppDbContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace June2026.Database.AppDbContextModels;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblStaff> TblStaffs { get; set; }
+
+    public virtual DbSet<TblStudent> TblStudents { get; set; }
+
+    public virtual DbSet<TblUser> TblUsers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=June2026DB;User ID=sa;Password=sasa@123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblStaff>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Tbl_Targaryen");
+
+            entity.ToTable("Tbl_Staff");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<TblStudent>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Student__32C52A795537D2FA");
+
+            entity.ToTable("Tbl_Student");
+
+            entity.Property(e => e.StudentId).HasColumnName("StudentID");
+            entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentNo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TblUser>(entity =>
+        {
+            entity.HasKey(e => e.UserId);
+
+            entity.ToTable("Tbl_User");
+
+            entity.Property(e => e.Password).IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ June2026.Database/AppDbContextModels/TblStaff.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace June2026.Database.AppDbContextModels;
+
+public partial class TblStaff
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
