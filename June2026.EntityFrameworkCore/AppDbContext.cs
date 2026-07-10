bc5a626f780ca1182d807d6b24c902229b146830
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2026-07-10 12:29:22 +0630

    feat: building AppDbContext to connect between the application and database

---
+++ June2026.EntityFrameworkCore/AppDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace June2026.EntityFrameworkCore
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder()
+                {
+                    DataSource = ".",
+                    InitialCatalog = "June2026DB",
+                    UserID = "sa",
+                    Password = "sasa@123",
+                    TrustServerCertificate = true
+                };
+                optionsBuilder.UseSqlServer(sb.ConnectionString);
+            }
+        }
+        public DbSet<UserEntity> Staffs {get; set;}
+        
+    }
+}
