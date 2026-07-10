75620abfc381d3f8da9d4c5718518f7dcf043ab3
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2026-07-10 21:47:15 +0630

    implement read only for Tbl_Customter

---
+++ June2026.EFCoreHomework.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//Read from Tbl_Customer
+using June2026.EFCoreHomework.Database.AppDbContextModels;
+
+HomeworkAppDbContext db = new HomeworkAppDbContext();
+List<TblCustomer> lst = db.TblCustomers.ToList();
+foreach (TblCustomer item in lst)
+{
+    Console.WriteLine(item.CustomerId);
+    Console.WriteLine(item.CustomerName);
+    Console.WriteLine(item.CustomerEmail);
+    Console.WriteLine(item.Phone);
+}
